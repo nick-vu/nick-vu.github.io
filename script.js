a4dc86ed41435780618a160dd48ac35c772aec74
nickvu <pvu33@gatech.edu>
2019-06-27 15:21:20 +0700

    Big change

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-var explButt = document.querySelector("#explore");
-var icon = document.querySelector("#icon")
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("menubutt").classList.add("d-none");
+}
 
-explButt.addEventListener("mouseover", function(){
-  explButt.style.backgroundColor = "#FF5A5F"
-  explButt.innerHTML = "Nick's Neuroscience and VR Blog"
-  icon.classList.remove("fade-out");
-  icon.classList.add("fade-in-slow");
-})
-
-explButt.addEventListener("mouseout", function(){
-  explButt.style.backgroundColor = "white"
-  explButt.innerHTML = "Click to Explore! Fuel Your Imagination...";
-  if (icon.classList.contains('is-paused')){
-  el.classList.remove('is-paused');
-} 
-  icon.classList.remove("fade-in-slow")
-  icon.classList.add("fade-out")
-
-
-})
-
-explButt.addEventListener("click", function(){
-  explButt.style.backgroundColor = "#FF5A5F";
-  explButt.innerHTML = "Nick's Neuroscience and VR Blog";
-  icon.classList.remove("fade-in-slow");
-  icon.classList.remove("fade-out");
-  icon.classList.add("fade-in");    
-})
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("menubutt").classList.remove("d-none");
+}
